77daab6868fc99b7edc07d9b0c82c7f04bab88fd
Guillaume Nadal <guillaume.nadal13@gmail.com>
2023-02-14 16:08:14 -0500

    add orthogonalisation

--- perceval/polyquantique/algorithm/DistributionEnvelope.py
+++ perceval/polyquantique/algorithm/DistributionEnvelope.py
@@ -104,3 +104,35 @@ def Lowdin(waves,x):
     new_base = np.array(np.matmul(S_sqrt_inv, waves))
     coeffs = S_sqrt
     return new_base, coeffs
+
+def qr_mgs_decompose(matrix: np.array):
+    """
+    For n x m matrix return Q1 and R1 components of QR decomposition using
+    the modified Gram-Schmidt process, where R1 is n x n upper triangular
+    and Q1 is m x n and have orthogonal columns.
+    """
+    n = matrix.shape[1]
+    q1 = np.array(matrix, dtype='float64')
+    r1 = np.zeros((n, n))
+    for k in range(n):
+        a_k = q1[..., k]
+        r1[k,k] = np.linalg.norm(a_k)
+        a_k /= r1[k, k]
+        for i in range(k+1, n):
+            a_i = q1[..., i]
+            r1[k,i] = np.transpose(a_k) @ a_i
+            a_i -= r1[k, i] * a_k
+    return q1, r1
+
+def characterize_basis(Q:np.ndarray)-> np.ndarray:
+    H = Q.T@Q
+    err_norm = np.max(np.abs(np.diag(H)-1))
+    
+    for i in range(H.shape[0]):
+        H[i,i] = 0
+    
+    err_orth = np.max(H)
+    
+    # print(f'Worse error from ||v_i|| = 1  condition is {err_norm}')
+    # print(f'Worse error from othoginality is {err_orth}')
+    return err_norm + err_orth
