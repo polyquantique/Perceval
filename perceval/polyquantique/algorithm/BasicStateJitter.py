c50e671cff430ab10da9f8386393fedbf680ae38
Guillaume Nadal <guillaume.nadal13@gmail.com>
2023-05-05 18:06:16 -0400

    final basic state

--- perceval/polyquantique/algorithm/BasicStateJitter.py
+++ perceval/polyquantique/algorithm/BasicStateJitter.py
@@ -9,45 +9,56 @@ import itertools
 
 
 class BasicStateJitter():
-    """_summary_
+    """Create a Basic with envelopes that can have offset in time
     """
 
-    def __init__(self,state:str,offset = None,source=None,size_vect = 10000):
-        """_summary_
+    def __init__(self,state,offset,source=None,methode='Gram_Schmidt'):
+        """Create a new orthogonal base for the envelopes and create the 
+        associate probabilities to be used in the ProcessorJitter
 
-        :param state: _description_
+        :param state: BasicState of a photon
         :type state: str
-        :param offset: _description_, defaults to None
-        :type offset: _type_, optional
-        :param source: _description_, defaults to None
-        :type source: _type_, optional
-        :param size_vect: _description_, defaults to 10000
-        :type size_vect: int, optional
-        :raises TypeError: _description_
+        :param offset: List of the offset of the photons. Must be the same lenght as state
+        :type offset: list, optional
+        :param source: The SourceJitter, defaults to None
+        :type source: SourceJitter, optional
+        :param methode: Methode d'orthogonalisation
+        :type methode: str, optional
         """
         
-        self.bs = pcvl.BasicState(state)  # set the basic state
-        if source is None:      # if no source is given create an exponential one
-            source = Source(["Dirac",1],["Exponential",0.5])# 2/90e-12
-        if offset is None:    #set jitter to 0 if no array is given
-            offset = np.zeros(self.bs.m)
+        self.bs = pcvl.BasicState(state)
+        if source is None:      
+            source = SourceJitter(["Dirac",1],["Exponential",0.5])
         if self.bs.m != np.array(offset).size:  # check if the jitter is the right size
-            raise TypeError("State and jitter must have the same dimension")
+            raise TypeError("State and offset must have the same lenght")
         self.offset = offset
-        self.source = source
-        self.size_vect_array = size_vect
-        self.space_array = np.linspace(self.source.temps[0],self.source.temps[1],self.size_vect_array)
-        self.vector_list = self.Vector_list(offset,source)
-        self.coef_matrix, self.vector_list_ortho, self.new_base= self.orthogonalisation(methode='Gram_Schmidt')
+        self.space_array = source.space_array
+        self.vector_list = self.Vector_list(source)
+        self.coef_matrix, self.vector_list_ortho, self.new_base= self.orthogonalisation(methode=methode)
         self.coef_list ,self.bs_vector =  self.make_states()
 
-    def orthogonalisation(self,methode='Gram_Schmidt'):
-        """_summary_
+    def Vector_list(self,source):
+        """Create the list of list that contain all the envelope. 
+        Only the one with at least 1 photon
 
-        :param methode: _description_, defaults to 'Gram_Schmidt'
+        :param source: The SourceJitter, defaults to None
+        :type source: SourceJitter
+        :return: list of list of the envelopes
+        :rtype: list
+        """
+        vector_list = np.zeros((self.bs.n,source.space_array.size))
+        jitter = self.offset[np.array(list(self.bs))==1]
+        for photon in range(self.bs.n):
+            vector_list[photon,:] = source.envelope_vector([source.space_array-jitter[photon]])
+        return vector_list
+
+    def orthogonalisation(self,methode='Gram_Schmidt'):
+        """Orthogonalise vector from vector list
+
+        :param methode: Methode use to orthogonalise, defaults to 'Gram_Schmidt'
         :type methode: str, optional
-        :return: _description_
-        :rtype: _type_
+        :return: coef_matrix,vector_list_ortho,new_base
+        :rtype: list,list,list
         """
         if methode=='Gram_Schmidt':
             new_base,n = qr_mgs_decompose(self.vector_list.T)[0],0
@@ -69,28 +80,14 @@ class BasicStateJitter():
 
         return coef_matrix,vector_list_ortho,new_base
     
-    def Vector_list(self,jitter,source):
-        """_summary_
 
-        :param jitter: _description_
-        :type jitter: _type_
-        :param source: _description_
-        :type source: _type_
-        :return: _description_
-        :rtype: _type_
-        """
-        vector_list = np.zeros((self.bs.n,self.space_array.size))
-        jitter = jitter[np.array(list(self.bs))==1]
-        for photon in range(self.bs.n):
-            vector_list[photon,:] = source.envelope_vector([self.space_array-jitter[photon]])
-        return vector_list
-
+    @ property
     def print_vect(self):
-        """_summary_
+        """Plot envelopes and the new base
         """
         plt.plot(self.space_array,self.vector_list.T)
         plt.ylabel('Amplitude normalisée')
-        plt.xlabel('Temps (ns)')
+        plt.xlabel('Temps (s)')
         plt.title('Enveloppe des photons')
         plt.show()
         plt.plot(self.space_array,self.new_base.T)
@@ -100,10 +97,11 @@ class BasicStateJitter():
         plt.show()
 
     def make_states(self):
-        """_summary_
+        """Create the new states as a list of list that correspond 
+        to eache entry with the associate property
 
-        :return: _description_
-        :rtype: _type_
+        :return: coef_list ,bs_vector
+        :rtype: list,list
         """
         idx_matrix = list(itertools.product(np.arange(self.bs.n),repeat=self.bs.n))
         coef_list = np.ones(len(idx_matrix))
@@ -129,21 +127,25 @@ class SourceJitter():
     photon and the distribution error of the source in count. It also take the range of the analyse.
     """
 
-    def __init__(self,distribution_list,envelope_list,range = [-10e-12,30e-12]):
+    def __init__(self,distribution_list,envelope_list,range = [-10e-12,30e-12],size_vect = 10000):
         f"""Put the first argument as the description and all the over as caracteristique
 
         :param distribution_list: ['type of distribution', param1,param2] can take Cauchy and Dirac distribution.
         :type distribution_list: list
         :param envelope_list: ['type of envelope', param1,param2] can take Exponential and Gaussian distribution.
         :type envelope_list: list
-        :param temps: The range of the analyse of the photon in seconds, defaults to [-10e-12,30e-12]
-        :type temps: list, optional
+        :param range: The range of the analyse of the photon in seconds, defaults to [-10e-12,30e-12]
+        :type range: list, optional
+        :param size_vect: Resolution of the envelope
+        :type size_vect: int, optional
         """
         self.distribution=distribution_list[0]
         self.distribution_arg = distribution_list[1:]
         self.envelope = envelope_list[0]
         self.envelope_arg = envelope_list[1:]
         self.range = range
+        self.size_vect_array = size_vect
+        self.space_array = np.linspace(range[0],range[1],self.size_vect_array)
 
     def distribution_jitter(self,size):
         """Return a list of offset according to the distribution used
@@ -156,7 +158,7 @@ class SourceJitter():
         if self.distribution == "Cauchy":
             return np.random.standard_cauchy(size)
         if self.distribution == "Dirac":
-            return [0]*size
+            return np.array([0]*size)
 
     def envelope_vector(self,x_list):
         """ Give the shape of the envelope with a list of abscissas 
